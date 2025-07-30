d66cc946a57e9162b0ebe1c8299ec6fc3d3c441f
Denys Chepiha <dchepiha@exoft.net>
2025-07-30 16:12:49 +0200

    Fixed logic of difficulty buttons

--- frontend/src/utils/getEffectiveDifficulty.ts
+++ frontend/src/utils/getEffectiveDifficulty.ts
@@ -2,8 +2,15 @@
 import type { UserStoreTypes } from "../types/userStore.types.ts";
 
 export const getEffectiveDifficulty = (base: UserStoreTypes["difficultyLevel"], round: number): UserStoreTypes["difficultyLevel"] => {
-    if (base === 'Hard') return 'Hard';
-    if (round >= 3) return 'Hard';
-    if (round >= 1) return 'Medium';
-    return 'Easy';
+
+    if (base === "Hard") return "Hard";
+
+    if (base === "Medium") {
+        return round >= 3 ? "Hard" : "Medium";
+    }
+
+    if (round >= 3) return "Hard";
+    if (round >= 1) return "Medium";
+
+    return "Easy";
 }
